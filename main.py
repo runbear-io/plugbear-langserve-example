6078df871c3ae41cc28662fec0806942d1682f88
Hoseong Liam Hwang <449334+thefron@users.noreply.github.com>
2023-12-15 12:49:38 +0900

    Update host

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ add_routes(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=int(
+    uvicorn.run(app, host="0.0.0.0", port=int(
         os.getenv("PORT", default=8000)))

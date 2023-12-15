a15ec8ff7585ee96a22be73a70ba426212c38e9d
Hoseong Liam Hwang <449334+thefron@users.noreply.github.com>
2023-12-15 11:01:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# PlugBear LangServe integration example
+
+You can use this example to deploy your own LangChain app using LangServe and connect to your Slack or Discord using PlugBear.
+
+## Prerequisites
+
+- poetry
+
+## Installation
+
+```bash
+poetry install
+```
+
+## Usage
+
+```bash
+poetry run python main.py
+```
---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from typing import Any, Dict
+from fastapi import FastAPI, HTTPException, Request
+from langchain.chat_models import ChatOpenAI
+from langserve import add_routes
+
+
+app = FastAPI()
+
+
+def verify_secret_key(config: Dict[str, Any], req: Request) -> Dict[str, Any]:
+    if req.headers.get("x-secret-key") != os.environ.get("SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Incorrect secret key")
+
+    return config
+
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/chat",
+    per_req_config_modifier=verify_secret_key,
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
